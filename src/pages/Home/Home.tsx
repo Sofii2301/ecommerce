76e53bec3559f5907f8b6b32f3b646ad94229e0b
Sofii2301 <sofialvarezrodero97@gmail.com>
2026-06-17 13:40:46 -0300

    feat: integrate products API into home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from "react"
 import { Hero } from "../../components/ui/Hero"
 
 const Home = () => {
+  const [products, setProducts] = useState([])
+  
+  const getProducts = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/results')
+      if (response) {
+        const data = await response.json()
+        setProducts(data)
+      } else {
+        throw new Error('Failed to fetch products');
+      }
+    } catch (error) {
+      throw new Error('Network error');
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
+  console.log(products)
+
   return (
-    <Hero />
+    <>
+      <Hero />
+      
+    </>
   )
 }
 
